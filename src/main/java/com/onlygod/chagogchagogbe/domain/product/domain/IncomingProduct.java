dd3b6a3f3fda23e6f27c786923f8f8799e39ee50
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-12 00:40:27 +0900

    🗂️ :: IncomingProduct

---
+++ src/main/java/com/onlygod/chagogchagogbe/domain/product/domain/IncomingProduct.java
@@ -0,0 +1,41 @@
+package com.onlygod.chagogchagogbe.domain.product.domain;
+
+import com.onlygod.chagogchagogbe.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class IncomingProduct extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(columnDefinition = "INT")
+    private Integer count;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Builder
+    public IncomingProduct(Integer count, Product product) {
+        this.count = count;
+        this.product = product;
+    }
+}
