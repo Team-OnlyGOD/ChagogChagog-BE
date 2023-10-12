2cbc5da3b7fbd19e20d1344982556a6a745b9f8c
gilgeunwoo <kesksjxhx@gmail.com>
2023-10-12 22:33:28 +0900

    🗂️ :: feign 세팅

--- src/main/java/com/onlygod/chagogchagogbe/ChagogChagogBeApplication.java
+++ src/main/java/com/onlygod/chagogchagogbe/ChagogChagogBeApplication.java
@@ -3,10 +3,12 @@ package com.onlygod.chagogchagogbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableFeignClients
 @SpringBootApplication
 public class ChagogChagogBeApplication {
 
